49987564624fb0434db8d215fac6b4585f7e4265
Peter Ching <peter.ching@gmail.com>
2017-04-10 00:20:30 -0400

    Add print_board()

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -3,10 +3,20 @@ import pdb
 def init_board(size=(3,3)):
     return [ [i for i in xrange(m*size[0], m*size[0]+size[0])] for m in xrange(0, size[1]) ]
 
+def print_board(board):
+    print "\n"
+    print "  " + "-" * (4 * len(board[0]) - 1)
+
+    for row in board:
+            print (" | " + " | ".join(["%d"] * len(row)) + " | ") % tuple(row)
+            print "  " + "-" * (4 * len(row) - 1)
+
+
+
 
 # pdb.set_trace()
 
 if __name__ == '__main__':
     # board = init_board((6, 10))
     board = init_board()
-    print board
+    print_board(board)
