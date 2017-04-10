28ef3a58d0c2e444e8a56bf6e7720e696f04c0a4
Peter Ching <peter.ching@gmail.com>
2017-04-09 23:56:54 -0400

    Add init_board().

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,3 +1,12 @@
 import pdb
 
-pdb.set_trace()
+def init_board(size=(3,3)):
+    return [ [i for i in xrange(m*size[0], m*size[0]+size[0])] for m in xrange(0, size[1]) ]
+
+
+# pdb.set_trace()
+
+if __name__ == '__main__':
+    # board = init_board((6, 10))
+    board = init_board()
+    print board
