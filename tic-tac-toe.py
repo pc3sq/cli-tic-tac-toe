a28a2bb9cb3fcf30a95a68212eb6612ee922514c
Peter Ching <peter.ching@gmail.com>
2017-04-14 19:36:25 -0400

    Change function name to init_game().

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -4,19 +4,27 @@ def init_board(size=(3,3)):
     return [ [i for i in xrange(m*size[0], m*size[0]+size[0])] for m in xrange(0, size[1]) ]
 
 def print_board(board):
-    print "\n"
     print "  " + "-" * (4 * len(board[0]) - 1)
 
     for row in board:
             print (" | " + " | ".join(["%d"] * len(row)) + " | ") % tuple(row)
             print "  " + "-" * (4 * len(row) - 1)
 
+def init_game():
+    num_of_players = 0
 
+    while num_of_players not in ["1", "2"]:
+        print "TIC-TAC-TOE"
+        num_of_players = raw_input("How many human players? (max of 2)\n")
+
+    board = init_board()
+
+    print_board(board)
+
+    print "End of start_game()."
 
 
 # pdb.set_trace()
 
 if __name__ == '__main__':
-    # board = init_board((6, 10))
-    board = init_board()
-    print_board(board)
+    init_game()
