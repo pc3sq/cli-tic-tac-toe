2871755e136b8a149f391aa842d7cd0be5a6f978
Peter Ching <peter.ching@gmail.com>
2017-04-14 19:45:00 -0400

    Create a Board class.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,30 +1,40 @@
 import pdb
 
-def init_board(size=(3,3)):
-    return [ [i for i in xrange(m*size[0], m*size[0]+size[0])] for m in xrange(0, size[1]) ]
 
-def print_board(board):
-    print "  " + "-" * (4 * len(board[0]) - 1)
+class Board(object):
+    """Proto for tic-tac-toe board."""
+    def __init__(self, size=(3,3)):
+        # super(, self).__init__()
+        self.size = size
+        self.status = self.init_board()
 
-    for row in board:
-            print (" | " + " | ".join(["%d"] * len(row)) + " | ") % tuple(row)
-            print "  " + "-" * (4 * len(row) - 1)
 
-def init_game():
-    num_of_players = 0
+    def init_board(self):
+        return [ [i for i in xrange(m * self.size[0], m * self.size[0] + self.size[0])] for m in xrange(0, self.size[1]) ]
 
-    while num_of_players not in ["1", "2"]:
-        print "TIC-TAC-TOE"
-        num_of_players = raw_input("How many human players? (max of 2)\n")
+    def print_board(self):
+        print "  " + "-" * (4 * len(self.status[0]) - 1)
 
-    board = init_board()
+        for row in self.status:
+                print (" | " + " | ".join(["%d"] * len(row)) + " | ") % tuple(row)
+                print "  " + "-" * (4 * len(row) - 1)
 
-    print_board(board)
+    def init_game(self):
+        num_of_players = 0
 
-    print "End of start_game()."
+        while num_of_players not in ["1", "2"]:
+            print "TIC-TAC-TOE"
+            num_of_players = raw_input("How many human players? (max of 2)\n")
+
+        # board = self.init_board()
+
+        self.print_board()
+
+        print "End of start_game()."
 
 
 # pdb.set_trace()
 
 if __name__ == '__main__':
-    init_game()
+    board = Board()
+    board.init_game()
