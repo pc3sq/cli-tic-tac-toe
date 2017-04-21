6a9053e72cc59e1a0ea35f4325e3a9295183202a
Peter Ching <peter.ching@gmail.com>
2017-04-20 23:13:22 -0400

    Add test file.

---
+++ test-tic-tac-toe.py
@@ -0,0 +1,9 @@
+import unittest
+import tic_tac_toe
+
+class TestBoard(unittest.TestCase):
+    def test_fail(self):
+        self.assertTrue(False)
+
+if __name__ == '__main__':
+    unittest.main()
